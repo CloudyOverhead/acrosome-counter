e495e6ac1da83c45179f7acf1059c9226fbdbcee
CloudyOverhead <jerome.simon@geolearn.ca>
2021-03-15 18:05:15 -0400

    Update hyperparameters

--- acrosome_counter/build_model.py
+++ acrosome_counter/build_model.py
@@ -8,7 +8,6 @@ from detectron2.config import get_cfg
 
 from acrosome_counter.inputs import QTY_CLASSES
 
-IMAGE_SHAPE = [1024, 1024, 3]
 LOG_DIR = join(".", "logs")
 PRETRAINED_CHECKPOINT = model_zoo.get_checkpoint_url(
     "COCO-Detection/faster_rcnn_R_101_FPN_3x.yaml"
@@ -24,17 +23,27 @@ def build_cfg(is_training, batch_size, learning_rate, qty_iters):
     )
     cfg.DATASETS.TRAIN = ("train",)
     cfg.DATASETS.TEST = ()
-    cfg.DATALOADER.NUM_WORKERS = 2
     cfg.INPUT.FORMAT = "RGB"
+    cfg.DATALOADER.NUM_WORKERS = 1
     cfg.OUTPUT_DIR = LOG_DIR
     if is_training:
         cfg.MODEL.WEIGHTS = PRETRAINED_CHECKPOINT
     else:
         cfg.MODEL.WEIGHTS = join(LOG_DIR, "model_final.pth")
+
     cfg.SOLVER.IMS_PER_BATCH = batch_size
     cfg.SOLVER.BASE_LR = learning_rate
     cfg.SOLVER.MAX_ITER = qty_iters
     cfg.SOLVER.STEPS = []
-    cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128
+
+    cfg.MODEL.ANCHOR_GENERATOR.ASPECT_RATIOS = [[1.0]]
+    cfg.MODEL.ANCHOR_GENERATOR.SIZES = [[16]]
+    cfg.MODEL.PIXEL_MEAN = [116.28, 103.53]
+    cfg.MODEL.PIXEL_STD = [1.0, 1.0]
+    cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 512
     cfg.MODEL.ROI_HEADS.NUM_CLASSES = QTY_CLASSES
+    cfg.MODEL.RPN.IN_FEATURES = ['p5', 'p6']
+
+    cfg.TEST.DETECTIONS_PER_IMAGE = 256
+
     return cfg

cbe01d0c841b7c0139fcc6ef6a73d1f63ab209fa
CloudyOverhead <jerome.simon@geolearn.ca>
2021-03-24 19:17:29 -0400

    Handle `m` appearing in Torch URL

--- setup.py
+++ setup.py
@@ -4,6 +4,16 @@ import os
 import sys
 from setuptools import setup, find_packages
 
+if os.name != 'nt':
+    raise OSError(
+        "Install this repository through `conda env update -n base --file "
+        "meta.yaml`."
+    )
+
+print(
+    "WARNING: Make sure you have installed CUDA 10.1 and Visual Studio "
+    "2019 manually, as well as `cudatoolkit=10.1` through `conda`."
+)
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -11,34 +21,23 @@ with open("README.md", "r") as fh:
 TORCH_URL = "https://download.pytorch.org/whl"
 
 ver = str(sys.version_info[0]) + str(sys.version_info[1])
+verm = ver + "m" if ver in ["36", "37"] else ver
 
 requirements = [
     "opencv-python",
     "matplotlib",
     "imgaug",
     "pandas",
+    (
+        f"torch @ {TORCH_URL}"
+        f"/cu101/torch-1.6.0%2Bcu101-cp{ver}-cp{verm}-win_amd64.whl"
+    ),
+    (
+        f"torchvision @ {TORCH_URL}"
+        f"/cu101/torchvision-0.7.0%2Bcu101-cp{ver}-cp{verm}-win_amd64.whl"
+    ),
+    "detectron2 @ git+https://github.com/DGMaxime/detectron2-windows.git",
 ]
-if os.name != 'nt':
-    raise OSError(
-        "Install this repository through `conda env update -n base --file "
-        "meta.yaml`."
-    )
-else:
-    print(
-        "WARNING: Make sure you have installed CUDA 10.1 and Visual Studio "
-        "2019 manually, as well as `cudatoolkit=10.1` through `conda`."
-    )
-    requirements += [
-        (
-            f"torch @ {TORCH_URL}"
-            f"/cu101/torch-1.6.0%2Bcu101-cp{ver}-cp{ver}m-win_amd64.whl"
-        ),
-        (
-            f"torchvision @ {TORCH_URL}"
-            f"/cu101/torchvision-0.7.0%2Bcu101-cp{ver}-cp{ver}m-win_amd64.whl"
-        ),
-        "detectron2 @ git+https://github.com/DGMaxime/detectron2-windows.git",
-    ]
 
 setup(
     name="acrosome-counter",
